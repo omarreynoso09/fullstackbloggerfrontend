d8f03b3137377862ca0bde4d30b09243cfbf2e03
Hansel Reynoso <omarreynoso09@gmail.com>
2022-07-07 11:26:12 -0400

    last commit

--- src/App.js
+++ src/App.js
@@ -2,44 +2,29 @@ import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
 import BlogsPage from "./Pages/Blogs";
 import { useState, useEffect } from "react";
-
 const urlEndpoint = "http://localhost:4000";
 
-function App() {
-  const [serverJSON, setServerJSON] = useState({ message: null });
-
-  // original
+const App = () => {
+  const [serverJSON, setServerJSON] = useState({ message: [] });
   useEffect(() => {
     const fetchData = async () => {
-      const apiResponse = await fetch(`${urlEndpoint}/blogs/hello-blogs`);
+      const apiResponse = await fetch(`${urlEndpoint}/blogs/all-blogs`);
       const apiJSON = await apiResponse.json();
       setServerJSON(apiJSON);
       return;
     };
     fetchData();
   }, []);
-
-  // code to copy and add
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const url = `${urlEndpoint}/blogs/all-blogs`
-  //     const apiResponse = await fetch(url);
-  //     const apiJSON = await apiResponse.json();
-  //     setServerJSON(apiJSON);
-  //     return;
-  //   };
-  //   fetchData();
-  // }, []);
-
   return (
     <div className="App">
       <Routes>
         <Route
           path="/blogs"
-          element={<BlogsPage message={serverJSON.message} />}
-        ></Route>
+          element={<BlogsPage blogs={serverJSON.message} />}
+        />
       </Routes>
     </div>
   );
-}
+};
+
 export default App;
--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -1,15 +1,45 @@
 import React from "react";
 
-const BlogsPage = (props) => {
+// const BlogsPage = (props) => {
+//   return (
+//     <div className="blogs-page">
+//       <h1>Blogs Page</h1>
+//       <p>
+//         Server Message:{" "}
+//         {props.message.map((blog) => {
+//           return <>{blog.title}</>;
+//         })}
+//       </p>
+//     </div>
+//   );
+// };
+const BlogsPage = ({ blogs }) => {
   return (
     <div className="blogs-page">
       <h1>Blogs Page</h1>
-      <p>
-        Server Message:{" "}
-        {props.message.map((blog) => {
-          return <>{blog.title}</>;
+      <div>
+        {blogs.map((blog) => {
+          return <BlogPost blog={blog} key={blog.id} />;
         })}
-      </p>
+      </div>
+    </div>
+  );
+};
+
+const BlogPost = ({ blog }) => {
+  return (
+    <div className="blogPost">
+      <h2>Title: {blog.title}</h2>
+      <br />
+      <h3>Author: {blog.author}</h3>
+      <br />
+      <h4>Category: {blog.category}</h4>
+      <br />
+      <div>{blog.text}</div>
+      <br />
+      <div>Created At: {blog.createdAt}</div>
+      <br />
+      <div>Last Modified: {blog.lastModified}</div>
     </div>
   );
 };
