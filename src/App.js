b9ca59e75a6f6ae69a68e20ecea28a921095c67c
Hansel Reynoso <omarreynoso09@gmail.com>
2022-07-07 21:42:30 -0400

    final update

--- src/App.js
+++ src/App.js
@@ -4,27 +4,56 @@ import BlogsPage from "./Pages/Blogs";
 import { useState, useEffect } from "react";
 const urlEndpoint = "http://localhost:4000";
 
-const App = () => {
+function App() {
+  //list of variables to sort and filter the blog data
+  //do not use empty () instead add "" strings
+  // for limit and page dont forget the Numberconstructor
+
   const [serverJSON, setServerJSON] = useState({ message: [] });
+  const [sortField, setSortField] = useState("title");
+  const [sortOrder, setSortOrder] = useState("ASC");
+  const [filterField, setFilterField] = useState("title");
+  const [filterValue, setFilterValue] = useState("");
+  const [limit, setLimit] = useState(Number());
+  const [page, setPage] = useState(Number(1));
+
   useEffect(() => {
     const fetchData = async () => {
-      const apiResponse = await fetch(`${urlEndpoint}/blogs/all-blogs`);
+      const url = `${urlEndpoint}/blogs/all-blogs?sortField=${sortField}&sortOrder=${sortOrder}&filterField=${filterField}&filterValue=${filterValue}&limit=${limit}&page=${page}`;
+      const apiResponse = await fetch(url);
       const apiJSON = await apiResponse.json();
       setServerJSON(apiJSON);
       return;
     };
     fetchData();
-  }, []);
+  }, [sortField, sortOrder, filterField, filterValue, limit, page]);
+
   return (
     <div className="App">
       <Routes>
         <Route
           path="/blogs"
-          element={<BlogsPage blogs={serverJSON.message} />}
-        />
+          element={
+            <BlogsPage
+              blogs={serverJSON.message}
+              sortField={sortField}
+              setSortField={setSortField}
+              sortOrder={sortOrder}
+              setSortOrder={setSortOrder}
+              filterField={filterField}
+              setFilterField={setFilterField}
+              filterValue={filterValue}
+              setFilterValue={setFilterValue}
+              limit={limit}
+              setLimit={setLimit}
+              page={page}
+              setPage={setPage}
+            />
+          }
+        ></Route>
       </Routes>
     </div>
   );
-};
+}
 
 export default App;
--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -1,22 +1,95 @@
 import React from "react";
 
-// const BlogsPage = (props) => {
-//   return (
-//     <div className="blogs-page">
-//       <h1>Blogs Page</h1>
-//       <p>
-//         Server Message:{" "}
-//         {props.message.map((blog) => {
-//           return <>{blog.title}</>;
-//         })}
-//       </p>
-//     </div>
-//   );
-// };
-const BlogsPage = ({ blogs }) => {
+const BlogsPage = ({
+  blogs,
+  sortField,
+  setSortField,
+  sortOrder,
+  setSortOrder,
+  filterField,
+  setFilterField,
+  filterValue,
+  setFilterValue,
+  limit,
+  setLimit,
+  page,
+  setPage,
+}) => {
   return (
     <div className="blogs-page">
       <h1>Blogs Page</h1>
+      <label>Sort Field</label>
+      &nbsp;
+      <select
+        value={sortField}
+        onChange={(event) => {
+          const newSortField = event.target.value;
+          setSortField(newSortField);
+        }}
+      >
+        <option value="title">Title</option>
+        <option value="author">Author</option>
+        <option value="createdAt">Created At</option>
+        <option value="id">ID</option>
+      </select>
+      <br />
+      <label>Sort Order</label>
+      &nbsp;
+      <select
+        value={sortOrder}
+        onChange={(event) => {
+          const newSortOrder = event.target.value;
+          setSortOrder(newSortOrder);
+        }}
+      >
+        <option value="ASC">Ascending</option>
+        <option value="DESC">Descending</option>
+      </select>
+      <br />
+      <label>Filter Field</label>
+      &nbsp;
+      <select
+        value={filterField}
+        onChange={(event) => {
+          const newFilterField = event.target.value;
+          setFilterField(newFilterField);
+        }}
+      >
+        <option value="title">Title</option>
+        <option value="author">Author</option>
+      </select>
+      <br />
+      <label>Filter Value: </label>
+      <input
+        type="text"
+        value={filterValue}
+        onChange={(event) => {
+          const newFilterValue = event.target.value;
+          setFilterValue(newFilterValue);
+        }}
+      ></input>
+      <label>Limit</label>
+      <input
+        type="number"
+        min="1"
+        value={limit}
+        placeholder="What is your limit, Daddy?"
+        onChange={(event) => {
+          const newLimit = event.target.value;
+          setLimit(newLimit);
+        }}
+      ></input>
+      <label>Page</label>
+      <input
+        type="number"
+        min="1"
+        value={page}
+        placeholder="Page"
+        onChange={(event) => {
+          const newPage = event.target.value;
+          setPage(newPage);
+        }}
+      ></input>
       <div>
         {blogs.map((blog) => {
           return <BlogPost blog={blog} key={blog.id} />;
@@ -25,7 +98,6 @@ const BlogsPage = ({ blogs }) => {
     </div>
   );
 };
-
 const BlogPost = ({ blog }) => {
   return (
     <div className="blogPost">
